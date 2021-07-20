10db5dc3149fa840e3c4c2edd0606d18814cb6fb
Petr Kozelka <pkozelka@gmail.com>
2021-07-20 21:44:45 +0200

    double csv reader - minor improvements

--- examples/doublecsv-reader.rs
+++ examples/doublecsv-reader.rs
@@ -26,19 +26,20 @@ fn main() -> std::io::Result<()> {
                 break;
             },
             _ => {
-                //panic!("Bad column count: {}", record.len()),
+                println!("Column count: {}", record.len());
                 break;
             }
         }
     }
-    println!("HEADERS: {:?}", record);
+    println!("(-- headers --)");
+    println!("{:?}", record);
     println!("(-- data --)");
-/*    while csv_reader.read_record(&mut record)? {
-        println!("ID: {}", record.get(0).unwrap())
-    }
-*/
     for r in csv_reader.records() {
-        println!("ID: {}", r?.get(0).unwrap())
+        let data = r?;
+        for (i,heading) in record.iter().enumerate() {
+            print!("{}: `{}`, ", heading, data.get(i).unwrap());
+        }
+        println!()
     }
     Ok(())
 }
