4c5623cde0da1f856bd1f8d1dee8d8069519a666
Petr Kozelka <pkozelka@gmail.com>
2021-07-20 02:37:24 +0200

    use enums for format

--- examples/get-by-id.rs
+++ examples/get-by-id.rs
@@ -1,9 +1,9 @@
-use fio_api_rs::FioClient;
+use fio_api_rs::{FioClient, ReportFormat};
 
 #[tokio::main(flavor="current_thread")]
 async fn main() {
     let token = std::fs::read_to_string(".git/fio-test-token").unwrap();
     let fio = FioClient::new(&token);
-    let result = fio.get_by_id(2020, 12, "csv".to_string()).await.unwrap();
+    let result = fio.get_by_id(2020, 12, ReportFormat::Csv).await.unwrap();
     println!("{}", result);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,57 @@
 
 use anyhow::Result; // TODO: use our own error enum
+use strum_macros::IntoStaticStr;
 
 type FioDatum = String;
 
-/// TODO: this must be an enum
-type FioFormat = String;
+/// 5.1 Supported transaction formats
+#[derive(IntoStaticStr)]
+pub enum TxFormat {
+    #[strum(serialize = "csv")]
+    Csv,
+    #[strum(serialize = "gpc")]
+    Gpc,
+    #[strum(serialize = "html")]
+    Html,
+    #[strum(serialize = "json")]
+    Json,
+    #[strum(serialize = "ofx")]
+    Ofx,
+    #[strum(serialize = "xml")]
+    FioXml,
+}
+
+/// 5.1 Supported report formats
+#[derive(IntoStaticStr)]
+pub enum ReportFormat {
+    #[strum(serialize = "csv")]
+    Csv,
+    #[strum(serialize = "gpc")]
+    Gpc,
+    #[strum(serialize = "html")]
+    Html,
+    #[strum(serialize = "json")]
+    Json,
+    #[strum(serialize = "ofx")]
+    Ofx,
+    #[strum(serialize = "xml")]
+    FioXml,
+    #[strum(serialize = "pdf")]
+    Pdf,
+    #[strum(serialize = "sta")]
+    Mt940,
+    /// `CAMT.053`
+    #[strum(serialize = "cba_xml")]
+    CbaXml,
+    /// `CAMT.053`
+    #[strum(serialize = "sba_xml")]
+    SbaXml,
+}
 
 /// TODO: think about better representation, ideally with header+payload structure
 type FioOutputStream = String;
 
+const URL_PREFIX: &'static str = "https://www.fio.cz/ib_api/rest";
 
 pub struct FioClient {
     token: String,
@@ -20,13 +63,14 @@ impl FioClient {
         Self { token: token.to_string() }
     }
 
-    pub fn get_periods(&self, datum_od: FioDatum, datum_do: FioDatum, format: FioFormat) -> Result<FioOutputStream> {
+    pub fn get_periods(&self, datum_od: FioDatum, datum_do: FioDatum, format: TxFormat) -> Result<FioOutputStream> {
         todo!()
     }
 
-    pub async fn get_by_id(&self, year: u16, id: u8, format: FioFormat) -> Result<FioOutputStream> {
-        // https://www.fio.cz/ib_api/rest/by-id/aGEMtmwcsg5EbfIjqIhunibjhuvfdtsersxexdtgMIdh6u3/2012/1/transactions.cba_xml
-        let url = format!("https://www.fio.cz/ib_api/rest/by-id/{token}/{year}/{id}/transactions.{format}",
+    pub async fn get_by_id(&self, year: u16, id: u8, format: ReportFormat) -> Result<FioOutputStream> {
+        let format: &'static str = format.into();
+        let url = format!("{url_prefix}/by-id/{token}/{year}/{id}/transactions.{format}",
+            url_prefix = URL_PREFIX,
             token = self.token,
             year = year,
             id = id,
@@ -38,7 +82,7 @@ impl FioClient {
             .text().await?)
     }
 
-    pub fn get_last(&self, format: FioFormat) -> Result<FioOutputStream> {
+    pub fn get_last(&self, format: TxFormat) -> Result<FioOutputStream> {
         todo!()
     }
 
@@ -50,7 +94,7 @@ impl FioClient {
         todo!()
     }
 
-    pub fn get_merchant(&self, datum_od: FioDatum, datum_do: FioDatum, format: FioFormat) -> Result<FioOutputStream> {
+    pub fn get_merchant(&self, datum_od: FioDatum, datum_do: FioDatum, format: ReportFormat) -> Result<FioOutputStream> {
         todo!()
     }
 
